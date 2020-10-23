bc56a7ec5e44611223920cda8b3e8b7de901ff26
Dyego Soriano <dyegodf2@gmail.com>
2020-10-23 10:26:35 -0300

    configurando prettier e eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
   },
   extends: [
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'prettier/standard',
+    'prettier/react',
     'plugin:react/recommended',
-    'standard'
+    'standard',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: 'module',
   },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
-  }
-}
+    'space-before-function-paren': 'off',
+    'prettier/prettier': 'error',
+    'react/prop-types': 'off',
+  },
+};
