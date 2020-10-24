8e4ffd507a60f8e45c3758c6026173442c1aeb4b
Dyego Soriano <dyegodf2@gmail.com>
2020-10-24 20:48:56 -0300

    add styled-components e estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+import GlobalStyle from './styles/global'
+
 export default function App() {
-  return <h1>Olá mundo!</h1>
+  return (
+    <>
+      <h1>Olá mundo!</h1>
+
+      <GlobalStyle />
+    </>
+  )
 }
